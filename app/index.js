a1cd0c05c470e5b1a244d6740c05a67210e88332
Sergey Rastrigin <s.rastrigin@gmail.com>
2017-04-16 14:16:14 +0300

    now use local-storage as store for initial state

--- app/index.js
+++ app/index.js
@@ -1,4 +1,35 @@
-import render from './routes';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {Provider} from 'react-redux';
+import storeFactory from './store';
+// AppContainer is a necessary wrapper component for HMR
+import {AppContainer} from 'react-hot-loader';
+import Routes from './routes';
+import * as sampleDate from './initialState.json';
+
+const initialState = (localStorage["redux-store"]) ?
+    JSON.parse(localStorage["redux-store"]) :
+    sampleDate;
+
+const saveState = () =>
+    localStorage["redux-store"] = JSON.stringify(store.getState());
+
+
+const store = storeFactory(initialState);
+store.subscribe(saveState);
+window.store = store;
+
+const render = () => {
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+                <Routes />
+            </Provider>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
 
 render();
 
