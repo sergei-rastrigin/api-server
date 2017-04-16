c6622925b2b11486c78ad25f7613ded7da774cc1
Sergey Rastrigin <s.rastrigin@gmail.com>
2017-04-16 14:12:15 +0300

    add reducers for profile page

--- app/store/reducers.js
+++ app/store/reducers.js
@@ -1,97 +1,155 @@
 import T from './actionTypes';
 import {combineReducers} from 'redux';
 
-const goal = (state = 10, action) =>
-    (action.type === T.SET_GOAL) ?
-        parseInt(action.payload) :
+
+const exp = (state = "", action) =>
+    (action.type === T.SET_EXP) ?
+        parseInt(action.data) :
         state;
 
-const skiDay = (state = null, action) =>
-    (action.type === T.ADD_DAY) ?
-        action.payload :
-        state;
-
-const errors = (state = [], action) => {
+const styles = (state = [], action) => {
     switch (action.type) {
-        case T.ADD_ERROR :
+        case T.ADD_STYLES:
+            // TODO write single style reducer
             return [
                 ...state,
-                action.payload
+                ...action.data
             ];
-        case T.CLEAR_ERROR :
-            return state.filter((message, i) => i !== action.payload)
-        default:
-            return state
-    }
-};
-
-const allSkiDays = (state = [], action) => {
-
-    switch (action.type) {
-
-        case T.ADD_DAY :
-
-            const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
-
-            return (hasDay) ?
-                state :
-                [
-                    ...state,
-                    skiDay(null, action)
-                ].sort((a, b) => new Date(b.date) - new Date(a.date))
-
-        case T.REMOVE_DAY :
-
-            return state.filter(skiDay => skiDay.date !== action.payload)
-
-        default:
-            return state
-    }
-
-};
-
-const fetching = (state = false, action) => {
-
-    switch (action.type) {
-
-        case T.FETCH_RESORT_NAMES :
-            return true;
-
-        case T.CANCEL_FETCHING :
-            return false;
-
-        case T.CHANGE_SUGGESTIONS :
-            return false;
-
-        default:
-            return state
-    }
-
-};
-
-const suggestions = (state = [], action) => {
-
-    switch (action.type) {
-
-        case T.CLEAR_SUGGESTIONS :
+        case T.CLEAR_STYLES:
             return [];
 
-        case T.CHANGE_SUGGESTIONS :
-            return action.payload;
-
-        default :
-            return state
+        default:
+            return state;
     }
-
 };
+
+const tatoos = (state = [], action) => {
+    switch (action.type) {
+        case T.ADD_TATOO:
+            // TODO write single tatoo reducer
+            return [...state, action.data];
+        case T.REMOVE_TATOO:
+            return state.filter(tatoo => tatoo._id !== action.data);
+        case T.CLEAR_TATOOS:
+            return [];
+        default: return state;
+    }
+};
+
+const flashes = (state = [], action) => {
+    switch (action.type) {
+        // TODO write single flash reducer
+        case T.ADD_FLASH:
+            return [...state, action.data];
+        case T.REMOVE_FLASH:
+            return state.filter(flash => flash._id !== action.data);
+        case T.CLEAR_FLASHES:
+            return [];
+        default: return state;
+    }
+};
+
+const firstName = (state = "", action) =>
+    (action.type === T.SET_FIRST_NAME) ?
+        action.data :
+        state;
+
+const lastName = (state = "", action) =>
+    (action.type === T.SET_LAST_NAME) ?
+        action.data :
+        state;
+
+const age = (state = 0, action) =>
+    (action.type === T.SET_AGE) ?
+        parseInt(action.data) :
+        state;
+
+const city = (state = "", action) =>
+    (action.type === T.SET_CITY) ?
+        action.data :
+        state;
+
+const description = (state = "", action) =>
+    (action.type === T.SET_DESCRIPTION) ?
+        action.data :
+        state;
+
+const address = (state = "", action) =>
+    (action.type === T.SET_ADDRESS) ?
+        action.data :
+        state;
+
+const mobile = (state = "", action) =>
+    (action.type === T.SET_MOBILE) ?
+        action.data :
+        state;
+
+const email = (state = "", action) =>
+    (action.type === T.SET_EMAIL) ?
+        action.data :
+        state;
+
+const sex = (state = "", action) =>
+    (action.type === T.SET_SEX) ?
+        action.data :
+        state;
+
+const avatar = (state = "", action) =>
+    (action.type === T.SET_AVATAR) ?
+        action.data :
+        state;
+
+const background = (state = "", action) =>
+    (action.type === T.SET_BACKGROUND) ?
+        action.data :
+        state;
+
+const facebook = (state = "", action) =>
+    (action.type === T.SET_SOCIAL_FACEBOOK) ?
+        action.data :
+        state;
+
+const vk = (state = "", action) =>
+    (action.type === T.SET_SOCIAL_VK) ?
+        action.data :
+        state;
+
+const instagram = (state = "", action) =>
+    (action.type === T.SET_SOCIAL_INSTAGRAM) ?
+        action.data :
+        state;
+
+const isMaster = (state = false, action) =>
+    (action.type === T.CHANGE_IS_MASTER) ?
+        action.data :
+        state;
+
 
 
 export default combineReducers({
-    allSkiDays,
-    goal,
-    errors,
-    resortNames: combineReducers({
-        fetching,
-        suggestions
+    profile: combineReducers({
+        masterInfo: combineReducers({
+            exp,
+            styles,
+            tatoos,
+            flashes
+        }),
+        isMaster,
+        firstName,
+        lastName,
+        age,
+        city,
+        description,
+        address,
+        mobile,
+        email,
+        sex,
+        avatar,
+        background,
+        social: combineReducers({
+            facebook,
+            vk,
+            instagram
+        })
     })
 });
